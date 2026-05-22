12c5bd0f1c041b22cd8fd81eec5616e603ff8e4b
Isaqu <zkdopix@gmail.com>
2026-05-21 23:58:37 -0300

    completed oN avaliations 5

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/api/scrape', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/', 'index.html'));
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 4000;
