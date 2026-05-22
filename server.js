f6a277eff0cc81efdc52b8f107bad442eee2ccfc
Isaqu <zkdopix@gmail.com>
2026-05-21 23:38:28 -0300

    completed oN avaliations

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Servidor rodando na rede local');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
